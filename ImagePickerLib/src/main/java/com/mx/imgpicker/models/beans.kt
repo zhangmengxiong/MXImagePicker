454bf0768bb2462f7a15bde818e38f85194a5544
zhangmengxiong <zmx_final@163.com>
2022-04-04 16:33:51 +0800

    Update beans.kt

--- ImagePickerLib/src/main/java/com/mx/imgpicker/models/beans.kt
+++ ImagePickerLib/src/main/java/com/mx/imgpicker/models/beans.kt
@@ -102,6 +102,7 @@ internal class MXDataSet {
         folderList.deleteObservers()
         selectFolder.deleteObservers()
         selectList.deleteObservers()
+        needCompress.deleteObservers()
     }
 }
 
